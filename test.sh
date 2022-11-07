c5e7962e0441c12799363fe20269d6a972d8c2d6
erurami <88187787+erurami@users.noreply.github.com>
2022-11-08 05:51:41 +0900

    change test.sh

--- test.sh
+++ test.sh
@@ -18,28 +18,9 @@ assert () {
     fi
 }
 
-assert 0 "return 0;"
-assert 123 "return 123;"
-assert 6 "return 1 + 2 + 3;"
-assert 85 "return 100 - 25 + 10;"
-assert 123 "return 100 + 50 - 27;"
-assert 123 "return 3 + (2 * (100 / 2 + 10));"
-assert 123 "return (3 * 3 - 3) * 3 / 2 * 11 + 3 * 8;"
-assert 123 "return - (3 * 3 - 3) * - 3 / 2 * 11 + 3 * + 8;"
-assert 2 "return 1 + + 1;"
-assert 1 "return - ( 3 * 3 - 3 ) * - 3 / 2 * 11 + 3 * + 8 == 123;"
-assert 1 "return (- ( 3 * 3 - 3 ) * - 3 / 2 * 11 + 3 * + 8 == 123) + (1 != 1);"
-assert 1 "return 1 != 0;"
-assert 1 "return 1 < 2;"
-assert 1 "return 1 <= 1;"
-assert 0 "return 1 < 1;"
-assert 1 "return 2 > 1;"
-assert 1 "return 1 >= 1;"
-assert 0 "return 1 > 1;"
+assert 1 "{return (- ( 3 * 3 - 3 ) * - 3 / 2 * 11 + 3 * + 8 == 123) + (1 != 1);}"
 
-assert 123 "a = 123;return a;"
-assert 123 "a = 10; b = 10; c = a * (b + 2); c + 1; return c + 3;"
-assert 123 "Ten = 10; Two = 2; Three = 3; result1 = Ten * (Ten + Two); result2 = result1 + Three; return result2;"
+assert 123 "{Ten = 10; Two = 2; Three = 3; result1 = Ten * (Ten + Two); result2 = result1 + Three; return result2;}"
 
 echo OK
 
